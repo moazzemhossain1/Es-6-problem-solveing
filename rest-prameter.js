3f837af84161dc1d4b252df26ed07a78214f5bcf
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-06 17:36:57 +0800

    find the discount value using rest prameter

---
+++ rest-prameter.js
@@ -0,0 +1,21 @@
+
+function calculetTotle(discount,...prices){
+    // console.log(prices);
+    let totle=0;
+
+    for(let price of prices){
+        // console.log(price)
+        totle=totle+price;
+        // console.log(totle)
+       
+        //  console.log(totle)
+         let discountPrice=totle / discount;
+          minePrice=totle-discountPrice
+       
+    }
+    return minePrice;
+   
+
+}
+let amount=calculetTotle(10,100,200,300,400,500,700);
+console.log(amount)
