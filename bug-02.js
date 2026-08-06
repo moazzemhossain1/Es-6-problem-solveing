9867545c7a243727c5662168d345444fb29a7a4e
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-06 17:08:26 +0800

    out put are bug thake karo

---
+++ bug-02.js
@@ -0,0 +1,14 @@
+// function recetGeneter(name,price,qut){
+//     return`${name} x ${qut} = ${price*qut} "Taka"`
+
+// }
+// console.log(recetGeneter("pen",20,3)
+// )
+
+
+
+function recetGeneter(name,price,qut){
+    return `${name} x${qut} = ${price*qut} "Taka"`
+
+}
+console.log(recetGeneter("pen",20,3))
